165f95b3475010870d66aab6a84485595b6e2872
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2024-09-21 20:03:52 +0530

    Add files via upload

---
+++ Day_22/q2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class Stack {
+    int top;
+    int arr[100];
+
+public:
+    Stack() {
+        top = -1;
+    }
+
+    void push(int val) {
+        if (top >= 99) {
+            cout << "Stack Overflow\n";
+            return;
+        }
+        arr[++top] = val;
+    }
+
+    int pop() {
+        if (top == -1) {
+            cout << "Stack Underflow\n";
+            return -1;
+        }
+        return arr[top--];
+    }
+
+    int peek() {
+        if (top == -1) {
+            cout << "Stack is Empty\n";
+            return -1;
+        }
+        return arr[top];
+    }
+
+    bool isEmpty() {
+        return top == -1;
+    }
+};
+
+int main() {
+    Stack stack;
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+
+    cout << "Top element: " << stack.peek() << endl;
+
+    stack.pop();
+    cout << "Top element after pop: " << stack.peek() << endl;
+
+    return 0;
+}
