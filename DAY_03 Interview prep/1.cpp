23a751cbec3e7b056450165c0b4bad7a5fc9fc46
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2024-11-06 22:57:49 +0530

    Create 1.cpp

---
+++ DAY_03 Interview prep/1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+bool containsDuplicate(vector<int>& nums) {
+    unordered_set<int> seen;
+    for (int num : nums) {
+        if (seen.find(num) != seen.end()) {
+            return true;
+        }
+        seen.insert(num);
+    }
+    return false;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 1};
+    cout << (containsDuplicate(nums) ? "true" : "false") << endl;
+    return 0;
+}
